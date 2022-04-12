222f505c950b569027e289cb7169dfc0f0b1d49e
Raulea <robert.raulea@bearingpoint.com>
2022-04-12 18:23:50 +0300

    UI binding with API GET & DELETE implemented

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,65 +1,73 @@
-import { Component,OnInit} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { User } from '../user';
 import { UserService } from '../user.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  public userCount: number = 0;
-  public users: User[] = [];
-  public editDialogVisible: boolean = false;
-  public userInEdit?: User;
-  public dialogType: "Add" | "Edit" = "Add";
-  public defaultUser: User = {
-    id: 0,
-    lastName: '',
-    firstName: '',
-    email: '',
-    userName: ''
-  };
-  
-  constructor(private readonly userService: UserService) { }
- 
+	public userCount: number = 0;
+	public users: User[] = [];
+	public editDialogVisible: boolean = false;
+	public userInEdit?: User;
+	public dialogType: "Add" | "Edit" = "Add";
+	public defaultUser: User = {
+		id: 0,
+		lastName: '',
+		firstName: '',
+		email: '',
+		userName: ''
+	};
 
-  async ngOnInit(): Promise <void>  {
-    console.log('START');
-    this.users = await this.userService.getUsers();
-    this.userCount = this.users.length;
-    console.log('STOP');
-  };
-  
-  handleDelete(user: User){
-    this.userService.deleteUser(user);
-  }
+	public constructor(private readonly userService: UserService) { }
 
-  showAddDialog(){
-    this.dialogType = "Add";
-    this.userInEdit = {...this.defaultUser};
-    this.userCount++;
-    this.userInEdit.id = this.userCount;
-    this.editDialogVisible = true;
-  }
+	ngOnInit() {
+		this.preparePosts();
+	}
 
-  showEditDialog(user: User){
-    this.dialogType = "Edit";
-    this.userInEdit = {...user};
-    this.editDialogVisible = true;
-  }
-  
-  handleAdd(addedUser: User): void {
-    this.userService.addUser(addedUser);
-    this.editDialogVisible = false;
-  }
+	private preparePosts(): void {
+		this.userService.getUsers().subscribe(
+			(value: User[]) => {
+				this.users = value;
+			},
+			(error: any) => console.log("ERROR preparePosts")
+		);
+	}
 
-  handleEdit(editedUser: User) {
-    this.userService.save(editedUser);
-    this.editDialogVisible = false;
-  }
-  
-  handleDialogCloseRequest(){
-    this.editDialogVisible = false;
-  }
+	handleDelete(user: User) {
+		this.userService.deleteUser(user).subscribe(
+			(user: User) => this.preparePosts()
+		)
+
+	}
+
+	showAddDialog() {
+		this.dialogType = "Add";
+		this.userInEdit = { ...this.defaultUser };
+		this.userCount++;
+		this.userInEdit.id = this.userCount;
+		this.editDialogVisible = true;
+	}
+
+	public showEditDialog(user: User) {
+		this.dialogType = "Edit";
+		this.userInEdit = { ...user };
+		this.editDialogVisible = true;
+	}
+
+	handleAdd(addedUser: User): void {
+		this.userService.addUser(addedUser);
+		this.editDialogVisible = false;
+	}
+
+	handleEdit(editedUser: User) {
+		this.userService.save(editedUser);
+		this.editDialogVisible = false;
+	}
+
+	handleDialogCloseRequest() {
+		this.editDialogVisible = false;
+	}
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,60 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from './user';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class UserService {
-  private users: User[] = [
-    {
-      id: 1,
-      lastName: 'Raulea',
-      firstName: 'Robert',
-      email: 'robert.raulea@bearingpoint.com',
-      userName: 'RauleaR',
-    },
-    {
-      id: 2,
-      lastName: 'Ursu',
-      firstName: 'Daniel',
-      email: 'ursu.daniel@bearingpoint.com',
-      userName: 'UrsuD',
-    },
-    {
-      id: 3,
-      lastName: 'Bucur',
-      firstName: 'Andreea',
-      email: 'bucur.andreea@bearingpoint.com',
-      userName: 'BucurA',
-    }
-  ];
 
-  constructor() { }
+	private users: User[] = [];
 
-  public getUsers(): Promise<User[]> { // Promise => ca un fel de promisiune
-    return new Promise(
-      (resolve, reject) => {
-        setTimeout(() => { resolve(this.users) }, 2000)
-      }
-    )
-  }
+	public readonly ROOT_URL = 'http://localhost:3000';
 
-  public deleteUser(user: User): void {
-    let index: number = this.users.indexOf(user);
-    this.users.splice(index, 1);
-  }
+	constructor(private readonly http: HttpClient) {
 
-  public addUser(user: User): void {
-    this.users.push(user);
-  }
+	}
 
-  public save(editedUser: User): void {
-    const foundUser: User | undefined = this.users.find((user) => user.id === editedUser.id);
-    if (foundUser) {
-      const index = this.users.indexOf(foundUser);
-      if (index >= 0) {
-        this.users.splice(index, 1, editedUser);
-      }
-    }
-  }
+	public getUsers(): Observable<User[]> { // Promise => ca un fel de promisiune
+		return this.http.get<User[]>(this.ROOT_URL + "/user");
+	}
+
+	// public deleteUser(user: User): void {
+	// 	let index: number = this.users.indexOf(user);
+	// 	this.users.splice(index, 1);
+	// }
+
+	public deleteUser(user: User): Observable<User> {
+		console.log(user.id);
+		return this.http.delete<User>(`${this.ROOT_URL}/user/${user.id}`);
+	}
+
+	public addUser(user: User): void {
+		this.users.push(user);
+	}
+
+	public save(editedUser: User): void {
+		const foundUser: User | undefined = this.users.find((user) => user.id === editedUser.id);
+		if (foundUser) {
+			const index = this.users.indexOf(foundUser);
+			if (index >= 0) {
+				this.users.splice(index, 1, editedUser);
+			}
+		}
+	}
 }

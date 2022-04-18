f38b92da7dd1c081ad3286ff7ee97016c9841d60
Raulea <robert.raulea@bearingpoint.com>
2022-04-18 15:52:16 +0300

    Nr of line per pag dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserDialogComponent } from './user-dialog/user-dialog.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,13 @@ import { UserDialogComponent } from './user-dialog/user-dialog.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+	BrowserAnimationsModule, // required animations module
+	ToastrModule.forRoot({
+		timeOut: 3000,
+		positionClass: 'toast-bottom-right',
+		preventDuplicates: true,
+	  }), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { User } from '../user';
 import { UserService } from '../user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, DoCheck {
+	public pageCount: number = 5;
+	public rowCountOptions: number[] = [5, 10, 15, 20, 100];
 	public userCount: number = 0;
 	public users: User[] = [];
+	public pagesNumber: number[] = [];
 	public editDialogVisible: boolean = false;
 	public userInEdit?: User;
 	public dialogType: "Add" | "Edit" = "Add";
@@ -21,27 +25,66 @@ export class HomeComponent implements OnInit {
 		userName: ''
 	};
 
-	constructor(private readonly userService: UserService) { }
-
+	constructor(private readonly userService: UserService, private toastr: ToastrService) { }
+	
 	public ngOnInit() {
 		this.loadUsers();
 	}
+
+	ngDoCheck(): void {
+		this.showPages();
+	}
+
+	public showPages(): number[]{
+		this.pagesNumber = [];
+		for(let i = 1; i <= Math.floor(this.users.length / this.pageCount); i++){
+			this.pagesNumber.push(i);
+		}
+		if((this.users.length % this.pageCount) !== 0 ){
+			this.pagesNumber.push(this.pagesNumber.length + 1)
+		}
+		return this.pagesNumber
+	}
+	
+	public changePageEvent(){
+		
+	}
+
+	public pageCountFcn(n:number): void{
+		this.pageCount = n;
+	}
+
+	public showSuccesToaster(message: string) {
+		this.toastr.success(message, 'Succes!', {
+			closeButton: false,
+		});
+	}
+
+	public showErrorToaster(message: string) {
+		this.toastr.error(message, 'Error!', {
+			closeButton: false,
+		});
+	}
+
 
 	private loadUsers(): void {
 		this.userService.getUsers().subscribe(
 			(value: User[]) => {
 				this.users = value;
 			},
-			(error: any) => console.log("ERROR preparePosts")
+			(error: any) => this.showErrorToaster("Error 500")
 		);
 	}
 
 	public handleDelete(user: User) {
 		this.userService.deleteUser(user).subscribe(
-			(user: User) => this.loadUsers(),
-			(error: any) => console.log("ERROR deleteUser")
+			(user: User) => {
+				this.loadUsers();
+				this.showSuccesToaster(`${user.firstName}` + ' ' + "was deleted")
+			},
+			(error: any) => this.showErrorToaster("Error 500")
+
 		)
-		console.log(this.loadUsers());
 	}
 
 	public showAddDialog() {
@@ -60,17 +103,23 @@ export class HomeComponent implements OnInit {
 
 	public handleAdd(addedUser: User): void {
 		this.userService.addUser(addedUser).subscribe(
-			(user: User) => this.loadUsers(),
-			(error: any) => console.log('ERROR AddUser')
+			(user: User) => {
+				this.loadUsers();
+				this.showSuccesToaster("Added");
+			},
+			(error: any) => this.showErrorToaster("Error 500")
 		);
 		this.editDialogVisible = false;
 	}
 
 	public handleEdit(editedUser: User) {
 		this.userService.edit(editedUser).subscribe(
-			(user: User) => this.loadUsers(),
-			(error: any) => console.log(error)
-			);
+			(user: User) => {
+				this.loadUsers();
+				this.showSuccesToaster("Edited");
+			},
+			(error: any) => this.showErrorToaster("Error 500")
+		);
 		this.editDialogVisible = false;
 	}
 
